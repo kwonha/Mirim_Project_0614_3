c307c48539c5e76aba925ba2be83c6353112ba34
kwonha <s2118@e-mirim.hs.kr>
2022-06-16 19:07:21 +0900

    complete

--- app/src/main/java/kr/hs/emirim/s2118/mirim_project_0614_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2118/mirim_project_0614_3/MainActivity.java
@@ -5,30 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper vFlip;
+    EditText editInterval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vFlip = findViewById(R.id.vflip);
-        Button btnPrev = findViewById(R.id.btn_prev);
-        Button btnNext = findViewById(R.id.btn_next);
 
-        btnPrev.setOnClickListener(btnListener);
-        btnNext.setOnClickListener(btnListener);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+        Button btnSecond = findViewById(R.id.btn_second);
+        editInterval = findViewById(R.id.edit_interval);
+        btnStart.setOnClickListener(btnListener);
+        btnStop.setOnClickListener(btnListener);
+        btnSecond.setOnClickListener(btnListener);
+        vFlip.setFlipInterval(1000);
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()){
-                case R.id.btn_prev:
-                    vFlip.showPrevious();
+                case R.id.btn_start:
+                    vFlip.startFlipping();
                     break;
-                case R.id.btn_next:
-                    vFlip.showNext();
+                case R.id.btn_stop:
+                    vFlip.stopFlipping();
+                    break;
+                case R.id.btn_second:
+                    int second = Integer.parseInt(editInterval.getText().toString());
+                    vFlip.setFlipInterval(second*1000);
                     break;
             }
         }
